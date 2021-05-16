3c11e27ac91b5fcdba19e10fbc31efb4bd8930c6
PauloAlbes8039 <pj38alves@gmail.com>
2021-05-15 21:13:15 -0300

    Definindo a interface da aplicação

--- lib/Home.dart
+++ lib/Home.dart
@@ -6,8 +6,63 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _listaTarefas = ["Ir ao mercado", "Estudar", "Exercício do dia"];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.purple,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.purple,
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Adicionar Tarefa"),
+                  content: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Digite a sua tarefa",
+                    ),
+                    onChanged: (text) {},
+                  ),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text("Cancelar"),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    FlatButton(
+                      child: Text("Salvar"),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                );
+              });
+        },
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: _listaTarefas.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(
+                    _listaTarefas[index],
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
