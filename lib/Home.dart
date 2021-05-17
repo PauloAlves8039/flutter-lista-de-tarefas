f6cc217b7e48673dfdd7bd874a349ca8315961c8
PauloAlbes8039 <pj38alves@gmail.com>
2021-05-17 16:10:18 -0300

    Removendo item com Dismissible

--- lib/Home.dart
+++ lib/Home.dart
@@ -60,6 +60,41 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Widget criarItemLista(context, index) {
+    final item = _listaTarefas[index]["titulo"];
+
+    return Dismissible(
+        key: Key(item),
+        direction: DismissDirection.endToStart,
+        onDismissed: (direction) {
+          _listaTarefas.removeAt(index);
+          _salvarArquivo();
+        },
+        background: Container(
+          color: Colors.red,
+          padding: EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Icon(
+                Icons.delete,
+                color: Colors.white,
+              ),
+            ],
+          ),
+        ),
+        child: CheckboxListTile(
+          title: Text(_listaTarefas[index]["titulo"]),
+          value: _listaTarefas[index]["realizada"],
+          onChanged: (valorAlterado) {
+            setState(() {
+              _listaTarefas[index]["realizada"] = valorAlterado;
+            });
+            _salvarArquivo();
+          },
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
     // _salvarArquivo();
@@ -109,26 +144,7 @@ class _HomeState extends State<Home> {
           Expanded(
             child: ListView.builder(
               itemCount: _listaTarefas.length,
-              itemBuilder: (context, index) {
-                return CheckboxListTile(
-                  title: Text(_listaTarefas[index]["titulo"]),
-                  value: _listaTarefas[index]["realizada"],
-                  onChanged: (valorAlterado) {
-                    setState(() {
-                      _listaTarefas[index]["realizada"] = valorAlterado;
-                    });
-                    _salvarArquivo();
-                  },
-                );
-
-                /*
-                return ListTile(
-                  title: Text(
-                    _listaTarefas[index]["titulo"],
-                  ),
-                );
-                */
-              },
+              itemBuilder: criarItemLista,
             ),
           )
         ],
